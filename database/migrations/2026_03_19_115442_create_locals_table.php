68a76f9e3b22961ec634cee70463cd0b55547433
AngelSiete <andraco660@gmail.com>
2026-03-19 13:17:23 +0100

    Add Local and Propietario models + controllers
    Introduce Local and Propietario Eloquent models and scaffold resource controllers with stubbed methods.

---
+++ database/migrations/2026_03_19_115442_create_locals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('locals', function (Blueprint $table) {
+            $table->id()->autoIncrement()->unique();
+            $table->string('name');
+            $table->string('city');
+            $table->string('direction');
+            $table->integer('musicianCapacity');
+            $table->boolean('hasEquipment');
+            $table->text('description');
+            $table->integer('hourlyRate');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('locals');
+    }
+};
