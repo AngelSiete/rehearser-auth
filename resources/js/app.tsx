fb59d39938542ace28f448205c34e3e72e7de5d7
AngelSiete <andraco660@gmail.com>
2026-03-26 13:59:00 +0100

    big changes to get the main layout everywhere in the app

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,6 +3,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { TooltipProvider } from '@/components/ui/tooltip';
+import MainLayout from '@/Layouts/mainLayout';
 import '../css/app.css';
 import { initializeTheme } from '@/hooks/use-appearance';
 
@@ -10,11 +11,19 @@ const appName = import.meta.env.VITE_APP_NAME || 'Rehearser';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) =>
-        resolvePageComponent(
+    resolve: async (name) => {
+        const page: any = await resolvePageComponent(
             `./pages/${name}.tsx`,
             import.meta.glob('./pages/**/*.tsx'),
-        ),
+        );
+
+        page.default.layout =
+            page.default.layout ||
+            ((page: React.ReactNode) => <MainLayout>{page}</MainLayout>);
+
+        return page;
+    },
+
     setup({ el, App, props }) {
         const root = createRoot(el);
 
--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,6 +1,6 @@
 import { Head, Link, usePage } from '@inertiajs/react';
 import Index from '@/pages/locals';
-import { dashboard, login, register } from '@/routes';
+
 type LocalType = {
     id: number;
     name: string;
@@ -12,13 +12,10 @@ type LocalType = {
     hasEquipment: boolean;
 };
 export default function Welcome({
-    canRegister = true,
     locals
 }: {
-    canRegister?: boolean;
     locals: LocalType[];
 }) {
-    const { auth } = usePage().props;
 
     return (
         <>
@@ -30,35 +27,6 @@ export default function Welcome({
                 />
             </Head>
             <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
-                <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
-                    <nav className="flex items-center justify-end gap-4">
-                        {auth.user ? (
-                            <Link
-                                href={dashboard()}
-                                className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                            >
-                                Dashboard
-                            </Link>
-                        ) : (
-                            <>
-                                <Link
-                                    href={login()}
-                                    className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
-                                >
-                                    Login
-                                </Link>
-                                {canRegister && (
-                                    <Link
-                                        href={register()}
-                                        className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                                    >
-                                        Crea tu cuenta
-                                    </Link>
-                                )}
-                            </>
-                        )}
-                    </nav>
-                </header>
                 <h2 className="text-3xl text-white">
                     Nuestros locales más recientes
                 </h2>
