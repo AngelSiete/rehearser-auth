9b2fc6ae974e772787bd9768fbce3dcf08caabaf
AngelSiete <andraco660@gmail.com>
2026-03-26 14:23:42 +0100

    Update mainLayout.tsx

--- resources/js/layouts/mainLayout.tsx
+++ resources/js/layouts/mainLayout.tsx
@@ -10,8 +10,8 @@ export default function MainLayout({
 
     return (
         <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:p-8 dark:bg-[#0a0a0a]">
-            <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
-                <nav className="flex items-center justify-end gap-4">
+            <header className="mb-6 w-full text-sm not-has-[nav]:hidden lg:max-w-4xl">
+                <nav className="flex items-center justify-between gap-4">
                     {auth.user ? (
                         <Link
                             href={dashboard()}
@@ -26,6 +26,12 @@ export default function MainLayout({
                                 className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
                             >
                                 Login
+                            </Link>
+                            <Link
+                                href="/"
+                                className="inline-block px-5 py-1.5 text-xl leading-normal text-[#1b1b18] hover:border-[#19140035] hover:text-red-400 dark:text-[#EDEDEC] dark:hover:text-red-400 hover:text-2xl"
+                            >
+                                Rehearser
                             </Link>
                             {canRegister && (
                                 <Link
@@ -42,16 +48,16 @@ export default function MainLayout({
 
             <main className="w-full max-w-4xl">{children}</main>
             <div className="hidden h-14.5 lg:block"></div>
-            <footer className="flex justify-around w-2/4 m-auto flex-col md:flex-row">
+            <footer className="m-auto flex w-2/4 flex-col justify-around md:flex-row">
                 <Link
                     href="/faq"
-                    className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-m leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                    className="text-m inline-block rounded-sm border border-[#19140035] px-5 py-1.5 leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                 >
                     Preguntas frecuentes
                 </Link>
                 <Link
                     href="/somos"
-                    className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-m leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                    className="text-m inline-block rounded-sm border border-[#19140035] px-5 py-1.5 leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                 >
                     ¿Quiénes somos?
                 </Link>
