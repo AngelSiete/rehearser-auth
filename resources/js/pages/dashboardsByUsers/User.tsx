15f26affc773f41a38298867598ec03a93c9c1f4
AngelSiete <andraco660@gmail.com>
2026-04-05 18:10:25 +0200

    Update User.tsx

--- resources/js/pages/dashboardsByUsers/User.tsx
+++ resources/js/pages/dashboardsByUsers/User.tsx
@@ -20,14 +20,24 @@ const UserDashboard: React.FC<UserDashboardProps> = ({ auth, bookings }) => {
         cleanup();
         router.flushAll();
     };
-    console.log(bookings)
+
     return (
         <div className="p-6">
-            <pre>{JSON.stringify(bookings, null, 2)}</pre>
             <h1 className="mb-4 text-2xl font-bold">
                 Panel de Usuario - Músico
             </h1>
             <p className="mb-4">Sé bienvenido, {auth.user.name}! músico.</p>
+            <h2>Mis reservas</h2>
+            <ul>
+                {bookings.map((booking) => (
+                    <li key={booking.id}>
+                        <Link href={`/local/${booking.local.id}`}>
+                        Reserva en el local <strong>{booking.local.name}</strong> para el día {' '}
+                        {booking.booking_date} en {booking.local.city}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
             <div className="space-y-2"></div>
             <Link
                 className="block w-full cursor-pointer"
