50470d9ee9e795237df8805a7cbe0e33dfab7bc7
AngelSiete <andraco660@gmail.com>
2026-04-05 14:30:20 +0200

    added info for additional pages

--- resources/js/pages/faq.tsx
+++ resources/js/pages/faq.tsx
@@ -1,9 +1,34 @@
 export default function Faq(){
     return (
         <>
-            <h1 className="text-6xl text-white m-auto w-full text-center">Preguntas frecuentes</h1>
-            <h2 className="text-4xl text-white mt-5">Ejemplo ?</h2>
-            <p className="text-white text-2xl mt-2">Respuesta ejemplo</p>
+            <h1 className="rehearser-title m-auto w-full text-center text-6xl ">
+                Preguntas frecuentes
+            </h1>
+            <h2 className="mt-6 text-4xl text-white">
+                ¿Tengo que pagar para usar Rehearser?
+            </h2>
+            <p className="mt-2 text-2xl text-white">
+                No, puedes usar todas las funcionalidades de Rehearser de forma
+                gratuita
+            </p>
+            <h2 className="mt-6 text-4xl text-white">
+                ¿Cómo puedo empezar a usar Rehearser?
+            </h2>
+            <p className="mt-2 text-2xl text-white">
+                Es tan fácil como acceder a la página de registro (con botón
+                arriba), crear tu cuenta y marcar si te registras como MÚSICO
+                USUARIO o como PROPIETARIO DE LOCALES. Ésto es muy importante.
+            </p>
+            <h2 className="mt-6 text-4xl text-white">
+                Soy dueño de local/es, ¿cómo puedo subirlos a la paltaforma?
+            </h2>
+            <p className="mt-2 text-2xl text-white">
+                Es muy sencillo: regístrate como PROPIETARIO y tendrás acceso a
+                la opción de subir locales, donde podrás detallar la información
+                específica de tus espacios. Rehearser se encarga de mostrarlo a
+                los usuarios y permitir reservas del espacio en distintas
+                fechas, para que no se produzcan reservas dobles ni problemas.
+            </p>
         </>
     );
 }
--- resources/js/pages/somos.tsx
+++ resources/js/pages/somos.tsx
@@ -1,10 +1,22 @@
 export default function Somos() {
     return (
         <>
-            <h1 className="m-auto w-full text-center text-6xl text-white">
+            <h1 className="rehearser-title m-auto w-full text-center text-6xl ">
                 Quiénes somos
             </h1>
-            <p className="mt-2 text-2xl text-white">Rehearser nace de una necesidad real: encontrar espacios</p>
+            <p className="mt-6 text-2xl text-white">
+                Rehearser nace de una necesidad real: <b>encontrar espacios</b>{' '}
+                <br />
+                Como músico amateur no es extraño encontrarse en la tesitura de
+                buscar un local en el que poder ensayar libremente, sin
+                preocupaciones por el ruido u otros impedimentos al tocar en una
+                casa. <br />
+                Sin embargo, pese a ser una situación tan común, no existía
+                ninguna plataforma que una distintos locales de ensayo, para
+                ofrecer al usuario registrado la posibilidad de filtrar por
+                diferentes parámetros. <br /> <br />
+                De ésta <b>necesidad</b> nace <b>REHEARSER</b>
+            </p>
         </>
     );
 }
